d3770dda9771c4c883d38cc29e5364beff0d5c93
Duong Tu Hieu <Tuhieu9a1.vip@gmail.com>
2022-05-17 23:13:59 +0700

    update

--- BookMain1/app/src/main/java/com/example/bookmain1/MainActivity.java
+++ BookMain1/app/src/main/java/com/example/bookmain1/MainActivity.java
@@ -29,11 +29,12 @@ public class MainActivity extends AppCompatActivity {
         //Tạo mới/mở CSDL
         db = SQLiteDatabase.openOrCreateDatabase("/data/data/com.example.bookmain1/MyBook.db", null);
         //Ta che hàm sau lại, ở những lần chạy sau.Vì ta không muốn tạo CSDL lại từ đầu
-                //TaoBangvathemDuLieu();
+        //TaoBangVaThemDuLieu();
         //ThemMoiSach(6,"Mạng máy tính", 50, 10000, "Đây là sách mạng máy tính");
-     //   CapNhap(2,"Dương Tư Hiệu", 60, 99999, "Đây là hiệu đẹp trai");
 
-        NapSachVaoListView();
+        NapSACHvaoListview();
+        //CapNhat(2,"Dương Tư Hiệu", 60, 99999, "Đây là hiệu đẹp trai");
+
         Button nutXoa =(Button) findViewById(R.id.btnxoa);
         Button nutThem =(Button) findViewById(R.id.btnthem);
         Button nutsua =(Button) findViewById(R.id.btnsua);
@@ -88,76 +89,97 @@ public class MainActivity extends AppCompatActivity {
 
     }
     void XoaSach(int ma){
-        String[] thamsSoTruyen= {String.valueOf(ma)};
-        int kq = db.delete("BOOKS","BookID=?",thamsSoTruyen);
-        if(kq == 0)
-            Toast.makeText(this, "Không xóa được", Toast.LENGTH_LONG).show();
+        String[] thamSoTruyen={String.valueOf(ma)};
+        int kq = db.delete("BOOKS","BookID=?",thamSoTruyen);
+        if (kq ==0)
+            Toast.makeText(this, "Không xóa  được",Toast.LENGTH_LONG).show();
         else
-            Toast.makeText(this, "Xóa thành công", Toast.LENGTH_LONG).show();
+        {
+            Toast.makeText(this, "Xóa thành công",Toast.LENGTH_LONG).show();
 
     }
-    void CapNhap(int maGoc,String tenMoi, int sotrangMoi,float giaMoi,String motaMoi){
-        String[] thamsSoTruyen= {String.valueOf(maGoc)};
+    }
+    void CapNhat(int maGOC, String tenMOI, int sotrangMOI, float giaMOI, String motaMOI)
+    {
+        String[] thamSoTruyen={String.valueOf(maGOC)};
         ContentValues row = new ContentValues();
-        row.put("BookName",tenMoi);
-        row.put("Page",sotrangMoi);
-        row.put("Price", giaMoi);
-        row.put("Description", motaMoi);
-
-       int kq = db.update("BOOKS",row,"BookID=?",thamsSoTruyen);
-        if(kq == 0)
-            Toast.makeText(this, "Không cập nhập được", Toast.LENGTH_LONG).show();
+        // put (key, value)     key=tên cột, value= giá trị
+        row.put("BookName", tenMOI);
+        row.put("Page", sotrangMOI);
+        row.put("Price", giaMOI);
+        row.put("Description", motaMOI);
+        //
+        int kq = db.update("BOOKS",row,"BookID=?",thamSoTruyen);
+        if (kq ==0)
+            Toast.makeText(this, "Không cập nhật được",Toast.LENGTH_LONG).show();
         else
-            Toast.makeText(this, "Cập nhập thành công", Toast.LENGTH_LONG).show();
-    }
-    void NapSachVaoListView() {
-        //Lấy tham chiếu đến Listview trong Layout
-        ListView listView =(ListView) findViewById(R.id.lvSach);
-        //Nguồn dữ liệu
-        ArrayList<String> dsSach = new ArrayList<String>();
-        // Mở database, chọn dữ liệu, đưa vào ArrayList
-                //----------
-        Cursor cs = db.rawQuery("SELECT * FROM BOOKS", null);
-        cs.moveToFirst();
-        while (true){
-            if(cs.isAfterLast()==true) break;
-            //Lấy mã sách
-            int ms = cs.getInt(0);
-            //Lấy tên sách
-             String ts = cs.getString(1);
-             //Lấy giá bán
-            float gb = cs.getFloat(3);
-            String dong = String.valueOf(ms)+ " " + ts +"  "+ String.valueOf(gb)+"$";
-            //Đưa vào arraylist
-            dsSach.add(dong);
-            cs.moveToNext();
+        {
+            Toast.makeText(this, "Cập nhật thành công",Toast.LENGTH_LONG).show();
         }
-        //Adapter
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,dsSach);
+    }
+    void NapSACHvaoListview() {
+        //1. Lấy tham chiếu đến Listview trong Layout
+        ListView listView = (ListView) findViewById(R.id.lvSach);
+        //2. Nguồn dữ liệu
+        // Mỗi phần tử là một String, gồm:mã+tên+giá
+        ArrayList<String>  dsSach = new ArrayList<String>();
+        // Mở DB, select dữ liệu, đưa vào ArratList
+        ///----------------
+        Cursor cs = db.rawQuery("SELECT * FROM BOOKS",null);
+        cs.moveToFirst();
+        // duyệt từng dòng (bản ghi), tính toán
+        while (true)
+        {
+            if (cs.isAfterLast()==true) break;
+            // Lấy mã sách
+            int ms = cs.getInt(0);  // Cột 0, ở dòng hiện tại
+            // Lấy tên sách
+            String tenSach = cs.getString(1);
+            // Lấy giá bán
+            Float gia=cs.getFloat(3);
+            // Nối lại, để đưa vào arraylist
+            String dong = String.valueOf(ms)+ " --- "+
+                    tenSach + "----" + String.valueOf(gia);
+            // đưa vào arrayList
+            dsSach.add(dong);
+            // Sang dòng (bản ghi tiếp)
+            cs.moveToNext();
+
+        }
+
+        ///---------------
+        //3. Adapter
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                dsSach);
         //Gắn vào Listview
         listView.setAdapter(adapter);
+        // Bắt và xử lý sự kiện
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String dongChon = dsSach.get(position);
-                //Xử lý tách lấy phần mã
-                int k = dongChon.indexOf(" "); // tìm vị trí xuất hiện đầu tiên của khoảng trắng
-                 String ma =   dongChon.substring(0,k);
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String dongChon = dsSach.get(i);
+                // có dạng:   8 -- Tên sách  -- ...
+                // Xử lý tách lấy phần mã
+                int k= dongChon.indexOf(" "); // Tìm vị trí xuất hiện đầu tiên của khoảng trắng
+                String ma = dongChon.substring(0, k);  // trích lấy phần mã
+                //test thử bằng cách gán lên textvew chọn
                 TextView edChon = (TextView) findViewById(R.id.edtMachon);
-                edChon.setText("ID: " +ma);
+                edChon.setText(ma);
             }
         });
     }
 
-    void TaoBangvathemDuLieu(){
-        //Xóa bản nếu đã có
-        String sqlXoaBang = "DROP TABLE IF EXISTS BOOKS";
+    void TaoBangVaThemDuLieu() {
+        //Lệnh tạo bảng
+        // sqlXoaBang nếu đã có
+        String sqlXoaBang ="DROP TABLE IF EXISTS BOOKS";
         db.execSQL(sqlXoaBang);
-        // Lệnh tạo bảng
-        String sqlTaoBang = "CREATE TABLE BOOKS( BookID integer PRIMARY KEY," + "Book text," +
-                "Page integer," +
-                " Price Float, " +
-                "Description text)";
+        String sqlTaoBang ="CREATE TABLE BOOKS(     BookID integer PRIMARY KEY, " +
+                "   BookName text, " +
+                "   Page integer, "+
+                "   Price Float, "+
+                "   Description text)";
         db.execSQL(sqlTaoBang);
         // Thêm bản ghi
         String sqlThem1= "INSERT INTO BOOKS VALUES(1, 'Java', 100, 9.99, 'Sách về java')";
