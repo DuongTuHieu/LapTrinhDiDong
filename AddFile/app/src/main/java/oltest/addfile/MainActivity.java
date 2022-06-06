5edf1f814bb3a6ed31ef7b4215531e846d17be89
Duong Tu Hieu <Tuhieu9a1.vip@gmail.com>
2022-06-06 09:54:01 +0700

    go

---
+++ AddFile/app/src/main/java/oltest/addfile/MainActivity.java
@@ -0,0 +1,60 @@
+package oltest.bai14nguyendinhchi;
+
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button Add = (Button) findViewById(R.id.OpenTabAddSong);
+        Button View = (Button) findViewById(R.id.ViewTabAddSong);
+
+        Add.setOnClickListener(OpenTabAddSong);
+        View.setOnClickListener(ViewTabAddSong);
+    }
+
+
+    //  nếu người dùng cho phép thì đọc và tải tệp.
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==1){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Đã cấp quyền", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Quyền chưa được cấp", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+
+    View.OnClickListener OpenTabAddSong = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent tab2 = new Intent(MainActivity.this, AddFile.class);
+            startActivity(tab2);
+        }
+    };
+
+    View.OnClickListener ViewTabAddSong = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent tab3 = new Intent(MainActivity.this,ViewFile.class);
+            startActivity(tab3);
+        }
+    };
+}
